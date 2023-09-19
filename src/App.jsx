e409447c5f946674c741cf9c6c673e620c34048d
dipukumar11 <sodiptokumar143@gmail.com>
2023-09-19 10:27:58 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-3xl bg-gray-400 font-3xl font-bold p-5'>Vite + React</h1>
+      
+    
+    </>
+  )
+}
+
+export default App
